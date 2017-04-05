5d8547a3c4b87014e8d9be2dee72008266d8af61
Ralf S. Engelschall <rse@engelschall.com>
2017-04-05 11:31:09 +0200

    initially place under version control

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# gemstone-linter-js
-Linter for JavaScript/ECMAScript
+
+<img src="https://rawgit.com/gemstonejs/gemstone-artwork/master/gemstone-logo-white.svg" width="300" align="right" alt=""/>
+
+Gemstone Linter JavaScript
+==========================
+
+<p/>
+<img src="https://nodei.co/npm/gemstone-linter-js.png?downloads=true&stars=true" alt=""/>
+<p/>
+<img src="https://david-dm.org/rse/gemstone-linter-js.png" alt=""/>
+
+About
+-----
+
+This is the Linter for JavaScript/ECMAScript files in the
+[Gemstone JavaScript Technology Stack](http://gemstonejs.com).
+It is based on [ESLint](http://eslint.org/).
+
+Copyright &amp; License
+-----------------------
+
+Copyright &copy; 2016-2017 [GemstoneJS Project](http://gemstonejs.com)<br/>
+Licensed under [Apache License 2.0](https://spdx.org/licenses/Apache-2.0)
+
---
+++ gemstone-linter-js.js
@@ -0,0 +1,67 @@
+/*
+**  GemstoneJS -- Gemstone JavaScript Technology Stack
+**  Copyright (c) 2016-2017 Gemstone Project <http://gemstonejs.com>
+**  Licensed under Apache License 2.0 <https://spdx.org/licenses/Apache-2.0>
+*/
+
+/*  load external requirements  */
+const path      = require("path")
+const ESLint    = require("eslint")
+
+/*  exported API function  */
+module.exports = async function (filenames, opts = {}, report = { sources: {}, findings: [] }) {
+    /*  setup ESLint CLI engine  */
+    let rules = {}
+    if (opts.env === "development")
+        rules["no-console"] = "off"
+    Object.assign(rules, opts.rules)
+    let engine = new ESLint.CLIEngine({
+        useEslintrc: false,
+        configFile:  require.resolve("gemstone-config-eslint/eslint.yaml"),
+        rules:       rules
+    })
+
+    /*  interate over all source files  */
+    let passed = true
+    if (typeof opts.progress === "function")
+        opts.progress(0.0, "linting JS: starting")
+    for (let i = 0; i < filenames.length; i++) {
+        /*  indicate progress  */
+        if (typeof opts.progress === "function")
+            opts.progress(i / filenames.length, `linting JS: ${filenames[i]}`)
+
+        /*  execute ESLint on given source file  */
+        let result = engine.executeOnFiles([ filenames[i] ])
+
+        /*  report linting results  */
+        if (result.errorCount > 0 || result.warningCount > 0) {
+            passed = false
+            result.results.forEach((file) => {
+                let filename = path.relative(process.cwd(), file.filePath)
+                report.sources[filename] = file.source
+                file.messages.forEach((msg) => {
+                    let [ ruleProc, ruleId ] = [ "eslint", msg.ruleId ]
+                    let message = msg.message
+                    if (ruleId === null && message.match(/^Parsing error:/)) {
+                        message = message.replace(/\n(?:.|\n)*$/, "")
+                        ruleProc = "eslint-babel"
+                        ruleId   = "*"
+                    }
+                    report.findings.push({
+                        ctx:      "JS",
+                        filename: filename,
+                        line:     msg.line,
+                        column:   msg.column,
+                        message:  message,
+                        ruleProc: ruleProc,
+                        ruleId:   ruleId
+                    })
+                })
+            })
+        }
+    }
+    if (typeof opts.progress === "function")
+        opts.progress(1.0, "")
+    return passed
+}
+
